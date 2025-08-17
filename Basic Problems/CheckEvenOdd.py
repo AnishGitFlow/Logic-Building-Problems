4b567803f29a2e0659672c22281ffcf1b3106006
Anish Chandlekar <163227286+AnishGitFlow@users.noreply.github.com>
2025-08-17 23:30:48 +0530

    Add files via upload

---
+++ Basic Problems/CheckEvenOdd.py
@@ -0,0 +1,13 @@
+def CheckEvenOdd(n):
+    rem = n % 2
+    if rem == 0:
+        return True
+    else:
+        return False
+    
+def main():
+    print(CheckEvenOdd(8))
+    print(CheckEvenOdd(45))
+
+if __name__ == "__main__":
+    main()
