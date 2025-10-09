73cc73c1ba0a5aa06c3974e2984216afbe169bab
Anish Chandlekar <163227286+AnishGitFlow@users.noreply.github.com>
2025-10-09 22:55:31 +0530

    Add files via upload

---
+++ 06_ClosestNumber.py
@@ -0,0 +1,8 @@
+# Find Closest to n and Divisible by m
+
+# Given two integers n and m (m != 0). Find the number closest to n 
+# and divisible by m. If there is more than one such number, then 
+# output the one having maximum absolute value.
+
+# Input : n = 13, m = 4
+# Output : 12
